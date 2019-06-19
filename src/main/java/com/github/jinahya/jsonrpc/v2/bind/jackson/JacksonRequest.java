08ea8ac509b6fc22a305e26f6e814aa3fcaaacd8
Jin Kwon <onacit@gmail.com>
2019-06-19 12:19:31 +0900

    updated

--- src/main/java/com/github/jinahya/jsonrpc/v2/bind/jackson/JacksonRequest.java
+++ src/main/java/com/github/jinahya/jsonrpc/v2/bind/jackson/JacksonRequest.java
@@ -1,5 +1,26 @@
 package com.github.jinahya.jsonrpc.v2.bind.jackson;
 
+/*-
+ * #%L
+ * jsonrpc-bind-jackson
+ * %%
+ * Copyright (C) 2019 Jinahya, Inc.
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ValueNode;
@@ -12,6 +33,7 @@ import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Supplier;
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class JacksonRequest extends RequestObject<JsonNode, ValueNode> {
 
     /**
