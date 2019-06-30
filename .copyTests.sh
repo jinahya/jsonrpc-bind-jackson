cdb33f8aaccc288f72f5a6353c922accd617908f
Jin Kwon <onacit@gmail.com>
2019-06-30 15:59:17 +0900

    updated

---
+++ .copyTests.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+dir=/com/github/jinahya/jsonrpc/bind/v2
+cp ././jsonrpc-bind/src/test/java$dir/BeanValidationTests.java src/test/java$dir
+cp ././jsonrpc-bind/src/test/java$dir/JacksonTests.java src/test/java$dir
