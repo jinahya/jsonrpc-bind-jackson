ba7b95d98d5487215fe00d7669ea077e364124bb
Jin Kwon <onacit@gmail.com>
2019-07-06 11:40:13 +0900

    Remove Maven Wrapper

--- .copyTests.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-dir=/com/github/jinahya/jsonrpc/bind/v2
-cp ././jsonrpc-bind/src/test/java$dir/BeanValidationTests.java src/test/java$dir
-cp ././jsonrpc-bind/src/test/java$dir/JacksonTests.java src/test/java$dir
